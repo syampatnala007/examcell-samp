f1d78460b7cd38b277f4b583f658b285789ea079
syampatnala007 <79990882+syampatnala007@users.noreply.github.com>
2021-10-03 15:49:01 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -182,8 +182,8 @@ router.post('/form',function(req,res) {
 		email='NA';
 		total=remuneration;
 		clgName='Aditya Engineering College';
-		/*var today = new Date();
-		var dd = String(today.getDate()).padStart(2, '0');
+		var today = new Date();
+		/*var dd = String(today.getDate()).padStart(2, '0');
 		var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 		var yyyy = today.getFullYear();
 
